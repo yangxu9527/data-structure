cfa1ead5662e42ad06c9a8e8a64467155beef350
yangxu <yangxu9527@foxmail.com>
2020-09-13 19:30:14 +0800

    feat: 选择和插入排序

--- src/main/java/com/bailiny/sort/InsertSort.java
+++ src/main/java/com/bailiny/sort/InsertSort.java
@@ -14,8 +14,22 @@ public class InsertSort {
     }
 
     public static void insertSort(int[] arr) {
-        // 从左向右遍历，如果发现最小的值则与遍历的当前值进行交换
-
+        // 从左向右遍历，当前值从右向左依次和前面一个值和前前个值进行比较，如果小于前面的值则依次后移
+        int insetVal;
+        int insertIndex;
+        for (int i = 1; i < arr.length; i++) {
+            // 1.定义待插入的值和索引
+            insetVal = arr[i];
+            insertIndex = i - 1;
+            // 2.依次和前面的进行比较，寻找插入的位置
+            while (insertIndex >= 0 && insetVal < arr[insertIndex]) {
+                // 当待插入的值小于当前值时表示没有找到待插入的位置,则后移
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+            // 退出循环时表示找到了位置
+            arr[insertIndex + 1] = insetVal;
+        }
     }
 
 }
